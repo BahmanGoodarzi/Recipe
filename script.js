bf1585d049c856c5fde5ebe1fa8cb0bf1d1ad9b0
bimi <bimihiddg@gmail.com>
2022-02-24 20:34:38 +0330

    recipe.js.meals

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-function getRandomMail(){
+const meall = document.querySelector(".meals");
 
+getRandomMeal();
+async function getRandomMeal(){
+    const raspData = await (await fetch("https://www.themealdb.com/api/json/v1/1/random.php")).json()
+    const randomMeal = raspData.meals[0]
+    addMael(randomMeal,true)
 }
 
-function getMaileById(){
+function addMael(mealData , random = false){
+    const meal = document.createElement("div");
+    meal.classList.add('meal');
 
+    meal.innerHTML =
+        `<div class="meal-header">
+            ${random ? `<span class="random">random recipe</span>` : ''}
+            <img src="${mealData.strMealThumb}" alt="${mealData.Meal}">
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn active"><i class="far fa-heart" aria-hidden="true"></i></button>
+        </div>`
+
+        meall.appendChild(meal)
 }
 
-function getMailsBySearch(){
 
+
+
+async function getMealeById(id){
+    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id)
 }
+
+
+
+
+async function getMealsBySearch(term){
+    const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term)
+}
+
